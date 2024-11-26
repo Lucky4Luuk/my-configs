5b15d58222ecf9914598fc0a7dedcb5b15e409f3
Luuk van Oijen <lazyluuk.channel@gmail.com>
2024-11-26 11:57:00 +0100

    Update install.sh

--- hyprland/install.sh
+++ hyprland/install.sh
@@ -11,5 +11,3 @@ sudo pacman -S hyprpolkitagent-git
 cp .bash_profile ~/
 cp hyprland.conf ~/.config/hyprland/
 cp hyprpaper.conf ~/.config/hyprland/
-
-echo "Reboot now! Wallpaper should be stored in ~/wallpaper.jpg"
