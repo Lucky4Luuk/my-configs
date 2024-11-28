61da6e07425ca854887d1a90618d75d80d7e1406
luuk <lazyluuk.channel@gmail.com>
2024-11-28 16:26:36 +0100

    forgot to push files

---
+++ hyprland/run_wofi.sh
@@ -0,0 +1,2 @@
+pkill wofi
+wofi --config ~/.config/wofi/config/config --style ~/.config/wofi/src/mocha/style.css --show drun -a
---
+++ hyprland/update_cfg.sh
@@ -0,0 +1,7 @@
+set -e
+
+mkdir -p ~/.config/hypr
+cp hyprland.conf ~/.config/hypr/
+cp hyprpaper.conf ~/.config/hypr/
+
+echo "Hyprland configs copied over!"
