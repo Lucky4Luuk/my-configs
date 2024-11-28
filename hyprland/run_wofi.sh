e3b88d864a3fe9c77cac57c9f02dd351afaef989
luuk <lazyluuk.channel@gmail.com>
2024-11-28 16:27:37 +0100

    made wofi toggle on button press

--- hyprland/run_wofi.sh
+++ hyprland/run_wofi.sh
@@ -1,2 +1,5 @@
-pkill wofi
-wofi --config ~/.config/wofi/config/config --style ~/.config/wofi/src/mocha/style.css --show drun -a
+if pgrep -x "wofi" > /dev/null; then
+    pkill wofi
+else
+    wofi --config ~/.config/wofi/config/config --style ~/.config/wofi/src/mocha/style.css --show drun -a
+fi
